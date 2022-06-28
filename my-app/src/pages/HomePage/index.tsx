d2cee8e40eaec86a6dfa28e8bdfd116c5fbe24f7
Eduardo Borges <alemaobrk1@gmail.com>
2022-06-28 00:27:30 -0300

    componentiza tabela, e implementa paginação

--- my-app/src/pages/HomePage/index.tsx
+++ my-app/src/pages/HomePage/index.tsx
@@ -1,136 +1,23 @@
-import { Container, Grid, Typography } from "@material-ui/core"
-import { useEffect, useState } from "react"
-import apiClient from "../../services/api-client"
-import { Drink } from "../../types/drink"
-import { Table, Header, useStyles } from "./styles"
-
-// TASKS
-
-// Paginate
-// transform to table 
-// use description in that contid table 
+import {
+  Container,
+  Typography,
+} from "@material-ui/core"
+import { DrinksTable } from "../../components/DrinksTable"
+import {  useStyles } from "./styles"
 
 export const HomePage = () => {
-  const [drinks, setDrinks] = useState([])
-
   const classes = useStyles()
-
-  const getDrinks = async () => {
-    try {
-      const response = await apiClient.get("")
-      setDrinks(response.data.sort((a: Drink, b: Drink) => b.abv - a.abv).slice(0, 500))
-    } catch {
-      window.alert("Opa, ocorreu um erro ao carregar a página. Por favor tente novamente. ")
-    }
-  }
-
-  useEffect(() => {
-    getDrinks()
-  }, [])
 
   return (
     <>
-      <Container>
+      <Container className={classes.container }>
         <Typography
-          className={classes.mt}
+          className={classes.my}
           variant="h3"
           component="h1" >
           The World 500 Most Alcoholic Drinks
         </Typography>
-        <Table>
-          <Grid container >
-            <Grid item xs={12} >
-              <Header container spacing={1}>
-                <Grid item xs={1} className={classes.alignCenter}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Rank
-                  </Typography>
-                </Grid>
-                <Grid item xs={3} className={classes.alignCenter}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Drink
-                  </Typography>
-                </Grid>
-                <Grid item xs={1} className={classes.alignCenter}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Alcoohol
-                  </Typography>
-                </Grid>
-                <Grid item className={classes.alignCenter} xs={2}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Country
-                  </Typography>
-                </Grid>
-                <Grid item className={classes.alignCenter} xs={2}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Category
-                  </Typography>
-                </Grid>
-                <Grid item className={classes.alignCenter} xs={3}>
-                  <Typography 
-                    display="block"
-                    noWrap variant="h6">
-                    Website
-                  </Typography>
-                </Grid>
-              </Header>
-            </Grid>
-            {
-              drinks.map((drink: Drink, index) => (
-                <Grid key={index} item xs={12} >
-                  <Grid container spacing={1} className={classes.row}>
-                    <Grid item xs={1} className={classes.alignCenter}>
-                      <Typography
-                        className={classes.textRow} color="textPrimary" variant="body2" >
-                        {index + 1}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={3} className={classes.alignCenter}>
-                      <Typography
-                        className={classes.textRow} color="textPrimary" variant="body2" >
-                        {drink.name}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={1} className={classes.alignCenter}>
-                      <Typography
-                        className={classes.textRow} color="textSecondary" variant="body2">
-                        {drink.abv.toFixed(2)}%
-                      </Typography>
-                    </Grid>
-                    <Grid item className={classes.alignCenter} xs={2}>
-                      <Typography
-                        className={classes.textRow} color="textSecondary" variant="body2">
-                        {drink.country || "Uninformed"}
-                      </Typography>
-                    </Grid>
-                    <Grid item className={classes.alignCenter} xs={2}>
-                      <Typography
-                        className={classes.textRow} color="textSecondary" variant="body2">
-                        {drink.category || "Uninformed"}
-                      </Typography>
-                    </Grid>
-                    <Grid item className={classes.alignCenter} xs={3}>
-                      <Typography
-                        className={classes.textRow} color="textSecondary" variant="body2">
-                        {drink.website || "Uninformed"}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              ))
-            }
-          </Grid>
-        </Table>
+        <DrinksTable />
       </Container>
     </>
   )
--- my-app/src/pages/HomePage/styles.ts
+++ my-app/src/pages/HomePage/styles.ts
@@ -1,39 +1,11 @@
-import { Grid, makeStyles, styled } from "@material-ui/core";
-
-export const Table = styled('table')({
-  marginTop: "2.5rem",
-  margin: "auto",
-  boxShadow: "1px 1px 10px 1px rgb(0,0,0,0.5)", 
-  overflowY: "hidden",
-  borderRadius: "0.25rem 0.25rem 0rem 0rem",
-})
-
-export const Header = styled(Grid)({
-  backgroundColor: "rgb(255, 123, 0)",
-  color: "white",
-  borderRadius: "0.25rem 0.25rem 0rem 0rem",
-  paddingTop: "0.4rem",
-  paddingBottom: "0.4rem",
-  textAlign: "center", 
-})
+import { makeStyles,} from "@material-ui/core";
 
 export const useStyles = makeStyles({
-  row: {
-    textAlign: "center", 
-    paddingTop: "0.7rem",
-    paddingBottom: "0.7rem",
-    marginTop:"3px",
-  }, 
-  alignCenter: {
-    display: "flex", 
-    alignItems: "center", 
-    justifyContent: "center"
-  }, 
-  textRow: {
-    fontWeight: "bold", 
-    wordBreak: "break-all"
-  }, 
-  mt: {
+  my: {
     marginTop: "3rem",
-  }
+    marginBottom: "3rem",
+  },
+  container: {
+    paddingBottom: "6rem"
+  }  
 })
